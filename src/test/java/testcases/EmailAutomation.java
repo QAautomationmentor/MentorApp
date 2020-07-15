1b7c8224a515b08a35331cddb688eebe5560b8ff
Aditya <sisodiyaaditya95@gmail.com>
2020-07-15 21:47:20 +0530

    india

---
+++ src/test/java/testcases/EmailAutomation.java
@@ -0,0 +1,32 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Utilities.Base;
+
+public class EmailAutomation extends Base {
+	public WebDriver driver;
+
+	@BeforeClass
+	public void tearUp() {
+		driver = initializeDriver();
+	}
+
+	@AfterClass
+	public void teardown() {
+		driver.close();
+	}
+
+	@Test(priority = 1)
+	public void openApplication() {
+		driver.get("https://accounts.google.com/signin/v2");
+	}
+
+	@Test(priority = 2)
+	public void login() {
+
+	}
+}
